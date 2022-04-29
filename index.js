92e312d82a766fac12fcc32e0e695b83edf79fcd
Mahmudulislamasif <mahmudulislamasif2000@gmail.com>
2022-04-29 22:54:21 +0600

    set server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port=process.env.PORT||5000
+const app=express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Running')
+})
+
+app.listen(port,()=>{
+    console.log('Listening',port)
+})
+
